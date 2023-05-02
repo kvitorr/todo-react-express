f3427f05144dfd908f5c6e5711339e1ec13d495e
kvitorr <kvitorrsantos@gmail.com>
2023-05-01 22:07:41 -0300

    Create Component: Home

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Task } from './Task'
+import List from './List'
+import Desktop from './Desktop'
+import { ListButton } from './ListButton'
+import { Header } from './Header'
+import { HomeWrapper } from './styles'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeWrapper>
+      <Header/>
+      <Desktop/>
+    </HomeWrapper>
   )
 }
 
