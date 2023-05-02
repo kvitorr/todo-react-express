cc83037716c7ec0f0d8882181a0abf34aec2096e
kvitorr <kvitorrsantos@gmail.com>
2023-05-01 22:07:51 -0300

    Att Global Styles

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -6,18 +6,18 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
+        font-family: 'Roboto', sans-serif;    
     }
     
     body {
         min-height: 100vh;
     }
 
-    #root {
+    /*#root {
         min-height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
         background-color: #3A54AA;
-    }
+    }*/
 `
